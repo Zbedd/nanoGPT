09620e7f44bfbe16c6dbfa29a213f04eac830fa5
Zachary Beddingfield <zbeddingfield123@gmail.com>
2025-07-17 01:28:20 -0400

    Added generate() and loss functions, random generation achieved

--- train.py
+++ train.py
@@ -56,18 +56,18 @@ def get_batch(split):
 
 xb, yb = get_batch('train')
 
-print('inputs: ')
-print(xb.shape)
-print(xb)
-print('targets:')
-print(yb.shape)
-print(yb)
-print('----')
-for b in range(batch_size): # batch dimension
-    for t in range(block_size): # time dimension
-        context = xb[b, :t+1].tolist()
-        target = yb[b, t].item()
-        print(f"when input is {context} the target: {target} ('{itos[target]}')")
+# print('inputs: ')
+# print(xb.shape)
+# print(xb)
+# print('targets:')
+# print(yb.shape)
+# print(yb)
+# print('----')
+# for b in range(batch_size): # batch dimension
+#     for t in range(block_size): # time dimension
+#         context = xb[b, :t+1].tolist()
+#         target = yb[b, t].item()
+#         print(f"when input is {context} the target: {target} ('{itos[target]}')")
         
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size):
@@ -75,11 +75,40 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size) # vocab_size x vocab_size
     
-    def forward(self, idx, targets):
+    def forward(self, idx, targets=None):
         # idx and targets are both (B,T) tensor of integers
         logits = self.token_embedding_table(idx) # (B,T,C) where C is vocab_size
-        return logits
+        
+        if targets is None:
+            loss = None
+            
+        else:
+            #Need logits to be (B*T, C) and targets to be (B*T,) for cross_entropy loss
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C) # (B*T, C)
+            targets = targets.view(B*T)
+            
+            loss = F.cross_entropy(logits, targets) # logits are (B,T,C), targets are (B,T)
+        
+        return logits, loss
+    
+    def generate(self, idx, max_new_tokens):
+        # idx is (B, T) array of indices in the current context
+        for _ in range(max_new_tokens):
+            # get the predictions
+            logits, loss = self(idx)
+            # focus only on the last time step
+            logits = logits[:, -1, :] # becomes (B, C)
+            # apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            # sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
+            # append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+        return idx
     
 m = BigramLanguageModel(vocab_size).to(device)
 out = m(xb, yb)
-print(out.shape) # (B,T,C)
+
+idx = torch.zeros((1, 1), dtype=torch.long, device=device) # starting context (just a single newline)
+print(decode(m.generate(idx, max_new_tokens=100)[0].tolist())) # generate 100 new characters from the model
